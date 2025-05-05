e913e609f255e3050572578e133806bf3030292d
anesths <anestos.archon@gmail.com>
2025-05-05 22:11:03 +0300

    I dont even know anymore

--- maketable.c
+++ maketable.c
@@ -5,37 +5,30 @@ int main(void) {
     int **table;
     int n = 22, m = 17, i, j, level =1;
 
-    // Allocate memory for the table
     table = (int**) malloc(n * sizeof(int*));
     for(i = 0; i < n; i++) {
         table[i] = (int*) malloc(m * sizeof(int));
     }
 
-    // Build symmetrical stage
+    // build standard room
     for(i = 0; i < n; i++) {
         for(j = 0; j < m; j++) {
-            table[i][j] = ' '; // Default fill (ASCII space)
-
-            // Top wall
+            table[i][j] = ' '; // fill all with space
             if(i == 0 && j >= 3 && j <= 13) {
-                table[i][j] = '#';
+                table[i][j] = '#'; //top
             }
-            // Left and right walls
             else if(i > 0 && i < 7 && (j == 3 || j == 13)) {
-                table[i][j] = '#';
+                table[i][j] = '#'; // left and right
             }
-            // Middle platform
             else if(i == 7 && ((j > 2 && j < 7) || (j > 9 && j < 14))) {
-                table[i][j] = '#';
+                table[i][j] = '#'; //midle
             }
-            // Bottom pillars
-            else if(i > 7 && i < 11 && (j == 6 || j == 10)) {
-                table[i][j] = '#';
+            else if(i > 7 && i < 11 && (j == 6 || j == 10)) { 
+                table[i][j] = '#'; //bottom
             }
         }
     }
-
-    for(int l =0; l<level ; l++){
+    for(int l =0; l<level ; l++){  //room generation depending the level
         int k =11;
         for(i=11; i<k+10; i++){
             for(j=0; j<m; j++){
--- showtable.c
+++ showtable.c
@@ -6,34 +6,35 @@
 
 int main(void) {
     int **table;
-    int n = 22, m = 17, i, j, level = 1, ishashtag = 0, luckn, luckm;
-
+    int n = 22, m = 17, i, j, level = 1, ishashtag = 0, herolist[4] = {'B', 'W', 'E', 0}, luckn, luckm, dif = 3, monsters_pos, monsters;
+    int hcount=0, health;
     srand(time(NULL));
-
     table = (int**) malloc(n * sizeof(int*));
     for(i = 0; i < n; i++) {
         table[i] = (int*) malloc(m * sizeof(int));
     }
-
+    for(i=0; i<4; i++){
+        if(herolist[i] != 0){
+            hcount++;
+            continue;
+        }
+        break;
+    }
+    // build standard room
     for(i = 0; i < n; i++) {
         for(j = 0; j < m; j++) {
-            table[i][j] = ' ';
-
-            // Top wall
-            if(i == 0 && j >= 3 && j <= 13){
-                table[i][j] = '#';
+            table[i][j] = ' '; // fill all with space
+            if(i == 0 && j >= 3 && j <= 13) {
+                table[i][j] = '#'; //top
             }
-            // Left and right walls
             else if(i > 0 && i < 7 && (j == 3 || j == 13)) {
-                table[i][j] = '#';
+                table[i][j] = '#'; // left and right
             }
-            // Middle platform
             else if(i == 7 && ((j > 2 && j < 7) || (j > 9 && j < 14))) {
-                table[i][j] = '#';
+                table[i][j] = '#'; //midle
             }
-            // Bottom pillars
-            else if(i > 7 && i < 11 && (j == 6 || j == 10)) {
-                table[i][j] = '#';
+            else if(i > 7 && i < 11 && (j == 6 || j == 10)) { 
+                table[i][j] = '#'; //bottom
             }
         }
     }
@@ -83,6 +84,7 @@ int main(void) {
     
     k=11;
 
+    //O PALIATZIS
     for(i=0; i<level; i++){
         for (j=0; j<2; j++) {
             luckm = (rand() % 5)+1;
@@ -102,14 +104,14 @@ int main(void) {
         }
         k+=10;
     }
-    
     k=11;
     for(i=0; i<level; i++){
         for (j=0; j<2; j++) {
             luckm = (rand() % 5)+11;
             luckn = (rand() % 9)+1;
-            // printf("%d %d\n", luckn, luckm); // Debugging linee
-            if(table[k+luckn][luckm] == '@'){
+            // printf("%d %d\n", luckn, luckm); // Debugging line
+            
+            if(table[k+luckn][luckm] == '@'){   // Kanei check to @, heroes, monsters
                 j--;
                 continue;
             }
@@ -118,19 +120,186 @@ int main(void) {
                 continue;
             }
             else{
-                table[luckn+k][luckm] = '@';
+                table[k+luckn][luckm] = '@';
             }
         }
         k+=10;
     }
+    // HEROES
+    for(i=0; i<level; i++){
+        for(j=0; j<hcount; j++){
+            luckm = (rand() % 9)+4;
+            luckn = (rand() % 6)+1;
+            if(table[luckn][luckm] != '.'){
+                j--;
+                continue;
+            }
+            else{
+                table[luckn][luckm] = herolist[j];
+            }
+        }
+    }
 
-    for(i = 1; i<4; i++){
+    k=11;
+    //monsters right side
+    for(i=0; i<level; i++){
+        switch(dif){  // deciding number of monsters
+            case 1:
+                monsters = (rand() % 2) +1; // Child's Play
+                break;
+            case 2:
+                monsters = (rand() % 4) +1; // Medium
+                break;
+            case 3:
+                monsters = (rand() % 6) +1; // Hurt me plenty	
+                break;
+        }
+        for (j=0; j<monsters; j++) {
+            luckm = (rand() % 5)+11;
+            luckn = (rand() % 9)+1;
+                        
+            if(table[k+luckn][luckm] >= '1' && table[k+luckn][luckm] <= 'Z'){   // Kanei check to @, heroes, monsters
+                j--;
+                continue;
+            }
+            else if(luckn+k == k+5 && luckm == 5){
+                j--;
+                continue;
+            }
+            else{
+                switch(dif){
+                case 1: // Child's Play
+                    health = (rand() % 3) +1;
+                        switch(health){
+                            case 1:
+                                health = '1';
+                                break;
+                            case 2:
+                                health = '2';
+                                break;
+                            case 3:
+                                health = '3';
+                                break;
+                        }
+                        break;
+                    case 2: // Medium
+                        health = (rand() % 3) +4;
+                        switch(health){
+                            case 4:
+                                health = '4';
+                                break;
+                            case 5:
+                                health = '5';
+                                break;
+                            case 6:
+                                health = '6';
+                                break;
+                        }
+                        break;
+                    case 3:
+                        health = (rand() % 3) +7;
+                        switch(health){
+                            case 7:
+                                health = '7';
+                                break;
+                            case 8:
+                                health = '8';
+                                break;
+                            case 9:
+                                health = '9';
+                                break;
+                        }
+                }
+                table[k+luckn][luckm] = health;
+            }
+        k+=10;
+        }
+    }
+    k=11;
+    //monsters left side
+    for(i=0; i<level; i++){
+        switch(dif){  // deciding number of monsters
+            case 1:
+                monsters = (rand() % 2) +1; // Child's Play
+                break;
+            case 2:
+                monsters = (rand() % 4) +1; // Medium
+                break;
+            case 3:
+                monsters = (rand() % 6) +1; // Hurt me plenty	
+                break;
+        }
+        for (j=0; j<monsters; j++) {
+            luckm = (rand() % 5)+1;
+            luckn = (rand() % 9)+4;
+            // printf("%d %d\n", luckn, luckm); // Debugging line
+            
+            if(table[k+luckn][luckm] >= '1' && table[k+luckn][luckm] <= 'Z' ){   // Kanei check to @, heroes, monsters
+                j--;
+                continue;
+            }
+            else if(luckn+k == k+5 && luckm == 5){
+                j--;
+                continue;
+            }
+            else{ 
+                switch(dif){
+                case 1: // Child's Play
+                    health = (rand() % 3) +1;
+                    switch(health){
+                        case 1:
+                            health = '1';
+                            break;
+                        case 2:
+                            health = '2';
+                            break;
+                        case 3:
+                            health = '3';
+                            break;
+                    }
+                    break;
+                case 2: // Medium
+                    health = (rand() % 3) +4;
+                    switch(health){
+                        case 4:
+                            health = '4';
+                            break;
+                        case 5:
+                            health = '5';
+                            break;
+                        case 6:
+                            health = '6';
+                            break;
+                    }
+                    break;
+                case 3:
+                    health = (rand() % 3) +7;
+                    switch(health){
+                        case 7:
+                            health = '7';
+                            break;
+                        case 8:
+                            health = '8';
+                            break;
+                        case 9:
+                            health = '9';
+                            break;
+                    }
+                default:
+                    health = '#';
+                }
+                table[k+luckn][luckm] = health;
+            }
+        }
+        k+=10;
+    }
+    for(i = 1; i<4; i++){ 
         table[7][6+i] = '.';
     }
 
     for (i = 12; i < n; i++) {
         for (j = 1; j < m; j++) {
-            if (table[i][j] != '#' && table[i][j] != '@') {
+            if(table[i][j] != '#' && table[i][j] != '@' && (table[i][j] <= '1' || table[i][j] >= 'Z')) {
                 table[i][j] = '.';
             }
         }
@@ -142,7 +311,7 @@ int main(void) {
     }
     printf("%c |", 'A' + k);
     printf("\033[0m\n");
-    printf( " %.3d |", 1);
+    printf(" %.3d |", 1);
     printf("\n");
     
     for(k=0; k<n; k++){ 
